c5c96abd76b727d52c9410da2cdb272118df8bbe
Guilherme <guilhermeabscontato@gmail.com>
2023-10-28 00:59:48 -0300

    Alterações para padronização das rotas

--- src/main/java/com/projeto_engenharia/projeto/controller/ProfessorController.java
+++ src/main/java/com/projeto_engenharia/projeto/controller/ProfessorController.java
@@ -3,9 +3,6 @@ package com.projeto_engenharia.projeto.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,34 +14,25 @@ import com.projeto_engenharia.projeto.professor.Professor;
 import com.projeto_engenharia.projeto.professor.ProfessorRepository;
 import com.projeto_engenharia.projeto.professor.ProfessorRequestDTO;
 import com.projeto_engenharia.projeto.professor.ProfessorResponseDTO;
-import com.projeto_engenharia.projeto.user.User;
-import com.projeto_engenharia.projeto.user.UserRepository;
-
-import jakarta.persistence.EntityNotFoundException;
-import jakarta.validation.Valid;
 
 
 @RestController
 @RequestMapping("professor")
 public class ProfessorController {
     @Autowired
-    private ProfessorRepository professorRepository;
-    
-    @Autowired
-    private UserRepository userRepository;
+    private ProfessorRepository repository;
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
     public List<ProfessorResponseDTO> getAll() {
-    	List<ProfessorResponseDTO> professores = professorRepository.findAll().stream().map(ProfessorResponseDTO::new).toList();
-        return professores;	
+        List<ProfessorResponseDTO> professores = repository.findAll().stream().map(ProfessorResponseDTO::new).toList();
+        return professores;
     }
     
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping
-    public ResponseEntity saveProfessor(@Valid @RequestBody ProfessorRequestDTO data){
-    	User user = userRepository.findById(data.id()).orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado"));
-    	Professor newProfessor = professorRepository.save(new Professor(data, user));
-        return ResponseEntity.status(HttpStatus.CREATED).body(newProfessor.getUser().getId());
+    public void saveProfessor(@RequestBody ProfessorRequestDTO data){
+        Professor professor = new Professor(data);
+        repository.save(professor);
     }
 }
